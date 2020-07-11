8f56c96384d05d50809d31bceedfee22041e9f38
Pratik Karki <karkipra@grinnell.edu>
2020-07-11 10:03:44 -0400

    fixing with the other pull request

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request, session, redirect
-from forms import LoginForm
 from flask_bootstrap import Bootstrap
 
 # Initializing app
 app = Flask(__name__)
 # Initializing bootstrap
 bootstrap = Bootstrap(app)
-
-import os
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
@@ -28,11 +23,6 @@ def index():
         }
     ]
     return render_template('index.html', title='Water Walkers', user=user, events=events)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
