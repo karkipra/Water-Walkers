8cfc9377404847f0bf67497fe00ce1cd6d4a6b12
Pratik Karki <karkipra@grinnell.edu>
2020-07-11 08:25:12 -0400

    added template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, session, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # stuff
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
