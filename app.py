5d58d04ec9052c8fd718ed09e7467146a06a0713
Pratik Karki <karkipra@grinnell.edu>
2020-07-11 11:17:12 -0400

    added calendar

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def index():
             'body': 'Description!'
         }
     ]
-    return render_template('index.html', title='Water Walkers', user=user, events=events)
+    return render_template('index.html', title='Water Walkers', user=user, events=events, calendar_view=False)
 
 # consider adding login_required aspect (http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/)
 @app.route('/login', methods=["GET", "POST"])
@@ -56,6 +56,27 @@ def register():
         notes = request.form.get("notes")
 
         return "TODO"
+
+
+@app.route('/calendar')
+def calendar():
+    return render_template('json.html')
+
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+    # You'd normally use the variables above to limit the data returned
+    # you don't want to return ALL events like in this code
+    # but since no db or any real storage is implemented I'm just
+    # returning data from a text file that contains json elements
+
+    with open("events.json", "r") as input_data:
+        # you should use something else here than just plaintext
+        # check out jsonfiy method or the built in json module
+        # http://flask.pocoo.org/docs/0.10/api/#module-flask.json
+        return input_data.read()
 
 if __name__ == '__main__':
     app.run(debug=True)
