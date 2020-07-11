621496f612a95d168011d97bd935abef6f35ffb7
Pratik Karki <karkipra@grinnell.edu>
2020-07-11 17:00:22 -0400

    fixed the issue with missing calendar

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_bootstrap import Bootstrap
 import sqlite3
+from datetime import datetime 
+import json
 
 # Initializing app
 app = Flask(__name__)
@@ -101,7 +103,7 @@ def register():
 
 @app.route('/calendar')
 def calendar():
-    return render_template('json.html')
+    return render_template('calendar.html')
 
 
 @app.route('/data')
@@ -113,10 +115,28 @@ def return_data():
     # but since no db or any real storage is implemented I'm just
     # returning data from a text file that contains json elements
 
+    # SQLite query to add username and password into database
+    conn = sqlite3.connect('database/reupdated.db')
+    db = conn.cursor()
+    events = db.execute("SELECT * FROM EVENTS")
+    conn.commit()
+
+    # Pretty sure all of this can be written better but at least it works
+    js = []
+
+    for event in events:
+        d = {
+            'title': event[1],
+            'start': event[3],
+            'end': event[4],
+            'url': event[5]
+        }
+        js.append(d)
+
+    with open('events.json', 'w') as outfile:
+        json.dump(js, outfile)    
+
     with open("events.json", "r") as input_data:
-        # you should use something else here than just plaintext
-        # check out jsonfiy method or the built in json module
-        # http://flask.pocoo.org/docs/0.10/api/#module-flask.json
         return input_data.read()
 
 @app.route('/add', methods=["GET", "POST"])
@@ -139,7 +159,7 @@ def add_event():
         db = conn.cursor()
 
         # SQLite query to add username and password into database
-        db.execute("INSERT INTO EVENTS (event_name, event_descrip) VALUES (?, ?)", (name, descrip,))
+        db.execute("INSERT INTO EVENTS (event_name, event_descrip, start, end, url) VALUES (?, ?, ?, ?, ?)", (name, descrip, start, end, url))
         conn.commit()
 
         return redirect("/")
